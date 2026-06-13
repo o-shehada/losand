69805d4d228a9ad0094361a7e02bfcf5524cb4cf
Omar Muhammad <o.shehada@ard.ly>
2026-06-13 16:18:48 +0200

    fix: provide manufacture csrf token

--- losand/www/los-andalus/manufacture.py
+++ losand/www/los-andalus/manufacture.py
@@ -7,6 +7,7 @@ def get_context(context):
 	context.no_cache = 1
 	context.lang = frappe.local.lang or "ar"
 	context.site_name = frappe.local.site
+	context.csrf_token = frappe.sessions.get_csrf_token()
 	context.boot = {
 		"user": frappe.session.user,
 		"lang": context.lang,
